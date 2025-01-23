45f38e43b5c7d21e4ff131a9877bceb714b042b4
dd <18211882344@163.com>
2025-01-23 15:39:39 +0800

    创建项目-1

---
+++ src/main/java/com/dd/spring/yunpicturebackend/YunPictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dd.spring.yunpicturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YunPictureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dd/spring/yunpicturebackend/YunPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dd.spring.yunpicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YunPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
